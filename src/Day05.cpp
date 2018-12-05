27037b0fc6e1bf5541125a0e39a818ca638d0cb6
Will Killian <william.killian@outlook.com>
2018-12-05 01:12:55 -0500

    Day 05

--- src/Day05.cpp
+++ src/Day05.cpp
@@ -1,12 +1,48 @@
 #include "Solution.hpp"
 
+#include <vector>
+
+#include <range/v3/algorithm/min.hpp>
+#include <range/v3/view/filter.hpp>
+#include <range/v3/view/iota.hpp>
+#include <range/v3/view/transform.hpp>
+
+namespace view = ranges::view;
+
+int
+react(std::string s) {
+  std::vector<char> l;
+  for (char c : s) {
+    if (l.empty()) {
+      l.push_back(c);
+      continue;
+    }
+    if (char last = l.back(); abs(last - c) == abs('A' - 'a')) {
+      l.pop_back();
+    } else {
+      l.push_back(c);
+    }
+  }
+  return l.size();
+}
+
 template <>
 template <bool part2>
 void
 Day<5>::solve(std::istream &is, std::ostream &os) {
-  is.good();
-  os << std::endl;
+  std::string s{std::istream_iterator<char>(is), {}};
+  int ans;
+  if constexpr (!part2) {
+    ans = react(s);
+  } else {
+    ans = ranges::min(view::iota('a', 'z') | view::transform([&](char C) {
+                        return react(s | view::filter([&](char c) { return tolower(c) != C; }));
+                      }));
+  }
+  os << ans << '\n';
 }
 
-template void Day<5>::solve<true>(std::istream&, std::ostream&);
-template void Day<5>::solve<false>(std::istream&, std::ostream&);
+template void
+Day<5>::solve<true>(std::istream &, std::ostream &);
+template void
+Day<5>::solve<false>(std::istream &, std::ostream &);
