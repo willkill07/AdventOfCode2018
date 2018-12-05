f5ef354cab683998bec9b2e9aab51fce71a8499b
Will Killian <william.killian@gmail.com>
2018-12-05 11:20:31 -0500

    Day 05

--- src/Day05.cpp
+++ src/Day05.cpp
@@ -1,43 +1,44 @@
 #include "Solution.hpp"
 
+#include <iterator>
 #include <vector>
 
 #include <range/v3/algorithm/min.hpp>
-#include <range/v3/view/filter.hpp>
-#include <range/v3/view/iota.hpp>
+#include <range/v3/view/indices.hpp>
+#include <range/v3/view/remove_if.hpp>
+#include <range/v3/view/repeat.hpp>
 #include <range/v3/view/transform.hpp>
-
-namespace view = ranges::view;
+#include <range/v3/view/zip_with.hpp>
 
 int
-react(std::string s) {
-  std::vector<char> l;
-  for (char c : s) {
-    if (l.empty()) {
-      l.push_back(c);
-      continue;
-    }
-    if (char last = l.back(); abs(last - c) == abs('A' - 'a')) {
-      l.pop_back();
+reaction(std::string s) {
+  int size = s.size() - 1;
+  auto i = std::next(std::begin(s));
+  for (auto j = std::next(i); j != std::end(s); ++j) {
+    if ((*i ^ *j) == 32) {
+      --i, size -= 2;
     } else {
-      l.push_back(c);
+      std::iter_swap(++i, j);
     }
   }
-  return l.size();
+  return size;
 }
 
 template <>
 template <bool part2>
 void
 Day<5>::solve(std::istream &is, std::ostream &os) {
-  std::string s{std::istream_iterator<char>(is), {}};
+  std::string s {std::istream_iterator<char>(is), {}};
+  s.insert(std::begin(s), '*');
   int ans;
   if constexpr (!part2) {
-    ans = react(s);
+    ans = reaction(s);
   } else {
-    ans = ranges::min(view::iota('a', 'z') | view::transform([&](char C) {
-                        return react(s | view::filter([&](char c) { return tolower(c) != C; }));
-                      }));
+    using namespace ranges::view;
+    auto strip = [](std::string const &s, char C) {
+      return remove_if(s, [=](char c) { return ::tolower(c) == C; });
+    };
+    ans = ranges::min(zip_with(strip, repeat(s), closed_iota('a', 'z')) | transform(reaction));
   }
   os << ans << '\n';
 }
