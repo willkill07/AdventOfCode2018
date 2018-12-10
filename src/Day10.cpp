9aa1dda18ef0e6e43e7f1eba23eaf3a4408eb7e7
Will Killian <william.killian@gmail.com>
2018-12-10 12:52:31 -0500

    Added OCR for Day 10

--- src/Day10.cpp
+++ src/Day10.cpp
@@ -3,54 +3,66 @@
 #include <limits>
 #include <set>
 #include <vector>
+#include <unordered_map>
 
-#include <range/v3/algorithm/min.hpp>
+#include <range/v3/algorithm.hpp>
 #include <range/v3/getlines.hpp>
 #include <range/v3/numeric.hpp>
+#include <range/v3/view/chunk.hpp>
 #include <range/v3/view/indices.hpp>
+#include <range/v3/view/repeat.hpp>
 #include <range/v3/view/transform.hpp>
+#include <range/v3/view/zip.hpp>
 
-using ranges::view::closed_iota;
-using ranges::view::transform;
+#include <iomanip>
 
-using Long = std::numeric_limits<long long>;
+std::unordered_map<long long, char> OCR = {
+  {0b111110'100001'100001'100001'111110'100001'100001'100001'100001'111110, 'B'},
+  {0b011110'100001'100000'100000'100000'100000'100000'100000'100001'011110, 'C'},
+  {0b011110'100001'100000'100000'100000'100111'100001'100001'100011'011101, 'G'},
+  {0b100001'100001'100001'100001'111111'100001'100001'100001'100001'100001, 'H'},
+  {0b000111'000010'000010'000010'000010'000010'000010'100010'100010'011100, 'J'},
+  {0b100000'100000'100000'100000'100000'100000'100000'100000'100000'111111, 'L'},
+  {0b100001'110001'110001'101001'101001'100101'100101'100011'100011'100001, 'N'},
+  {0b111110'100001'100001'100001'111110'100000'100000'100000'100000'100000, 'P'}
+};
 
 struct point {
   long long x, y;
-
-  bool
-  operator==(point const& p) const {
-    return x == p.x && y == p.y;
-  }
-
-  bool
-  operator<(point const& b) const {
-    if (y != b.y)
-      return y < b.y;
-    return x < b.x;
-  }
 };
 
 struct particle {
-  point pos, vel;
+  point pos{0,0}, vel{0,0};
+  static auto eval(int t) {
+    return [t](particle const& p) -> point {
+      return {p.pos.x + p.vel.x * t, p.pos.y + p.vel.y * t};
+    };
+  }
 };
 
 struct box {
-  long long xMin{Long::max()}, xMax{Long::min()}, yMin{Long::max()}, yMax{Long::min()};
-
-  long long
-  area() const {
-    return (xMax - xMin) * (yMax - yMin);
+  using lim = std::numeric_limits<long long>;
+  point min {lim::max(), lim::max()} , max {lim::min(), lim::min()};
+  long long width() const {
+    return std::abs(max.x - min.x + 1);
+  }
+  long long height() const {
+    return std::abs(max.y - min.y + 1);
+  }
+  long long area() const {
+    return width() * height();
+  }
+  static box update(box const& b, point const& p) {
+    auto [x, y] = p;
+    box n;
+    n.min = {std::min(b.min.x, x), std::min(b.min.y, y)};
+    n.max = {std::max(b.max.x, x), std::max(b.max.y, y)};
+    return n;
   }
 
-  template <typename Point>
-  void
-  update(Point const& p) {
+  unsigned index(point const& p) const {
     auto [x, y] = p;
-    xMin = std::min(xMin, x);
-    xMax = std::max(xMax, x);
-    yMin = std::min(yMin, y);
-    yMax = std::max(yMax, y);
+    return (x - min.x) + (y - min.y) * width();
   }
 };
 
@@ -59,54 +71,50 @@ template <bool part2>
 void
 Day<10>::solve(std::istream& is, std::ostream& os) {
   std::vector<particle> particles;
-  int tMax{0}, tMin{std::numeric_limits<int>::max()};
+  int tMax {0}, tMin {std::numeric_limits<int>::max()};
   for (auto const& line : ranges::getlines(is)) {
     particle p;
     sscanf(line.c_str(), "position=<%lld,%lld> velocity=<%lld,%lld>", &p.pos.x, &p.pos.y, &p.vel.x, &p.vel.y);
-    if (p.vel.y != 0) {
-      int ty = std::abs(p.pos.y / p.vel.y);
-      tMin = std::min(tMin, ty);
-      tMax = std::max(tMax, ty);
-    }
-    if (p.vel.x != 0) {
-      int tx = std::abs(p.pos.x / p.vel.x);
-      tMin = std::min(tMin, tx);
-      tMax = std::max(tMax, tx);
-    }
+    int x = std::abs(p.pos.x / p.vel.x);
+    int y = std::abs(p.pos.y / p.vel.y);
+    tMin = std::min({tMin, x, y});
+    tMax = std::max({tMax, x, y});
     particles.push_back(p);
   }
 
-  auto eval = [](int t) {
-    return [t](particle const& p) -> point { return {p.pos.x + p.vel.x * t, p.pos.y + p.vel.y * t}; };
+  using namespace ranges::view;
+
+  auto area = [](auto const& a) { return std::get<1>(a).area(); };
+  auto findMinTime = [&](auto t) {
+    return std::pair(t, ranges::accumulate(particles | transform(particle::eval(t)), box {}, &box::update));
   };
 
-  auto [minTime, rect] = ranges::min(closed_iota(tMin, tMax) | transform([&](auto t) {
-                                       box b;
-                                       for (auto const& p : particles | transform(eval(t))) {
-                                         b.update(p);
-                                       }
-                                       return std::pair(t, b);
-                                     }),
-                                     std::less<>{},
-                                     [](auto const& a) { return std::get<1>(a).area(); });
+  auto [minTime, rect] = ranges::min(closed_iota(tMin, tMax) | transform(findMinTime), std::less<>{}, area);
   if constexpr (part2) {
     os << minTime << '\n';
     return;
   }
-  std::set<point> loc(particles | transform(eval(minTime)));
-  os << '\n';
-  auto i = loc.begin();
-  for (auto const y : closed_iota(rect.yMin, rect.yMax)) {
-    for (auto const x : closed_iota(rect.xMin, rect.xMax)) {
-      if (i != loc.end() && *i == point{x, y}) {
-        os << '*';
-        ++i;
-      } else {
-        os << ' ';
+
+  std::vector<int> grid(rect.area(), 0);
+  auto eval = particle::eval(minTime);
+  for (auto const & p : particles) {
+    grid[rect.index(eval(p))] = 1;
+  }
+
+  std::array<long long, 8> chars = { 0 };
+  for (auto&& [y, row] : zip(indices, grid | chunk(rect.width()))) {
+    for (auto && [i, cells] : zip(indices, row | chunk(8))) {
+      for (auto && [x, c] : zip(indices, cells) | take(6)) {
+        if (c == 1) {
+          chars[i] |= static_cast<long long>(c) << (59 - (x + y * 6));
+        }
       }
     }
-    os << '\n';
   }
+  for (auto c : chars) {
+    os << OCR.at(c);
+  }
+  os << '\n';
 }
 
 template void
